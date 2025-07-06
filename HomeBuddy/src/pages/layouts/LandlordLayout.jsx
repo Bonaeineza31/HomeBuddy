4b9f90cace4deac6b3757fe702ecbf584ed598b4
Shina <m.atete@alustudent.com>
2025-07-06 08:57:59 +0200

    Revert "Delete unnecessary landlord dashboard"
    This reverts commit d0154b4508f40e51f5fe920b814c203106c8e9f0.
    
    revert delete changes

---
+++ HomeBuddy/src/pages/layouts/LandlordLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Sidebar from '../../components/Landlord/LandlordSidebar';
+import '../../styles/Landlord.css';
+
+const LandlordLayout = ({children}) => {
+  return (
+    <div className="landlord-layout">
+        <Sidebar />
+        <main className="landlord-main">
+            {children}
+        </main>
+    </div>
+  );
+};
+
+export default LandlordLayout;
